11e284f9e6d78356232cfa724efbcd3d3a81af05
Iago Cavalcante <iagoangelimc@gmail.com>
2018-07-25 15:55:39 -0300

    Build configuration

--- quasar.conf.js
+++ quasar.conf.js
@@ -153,13 +153,6 @@ module.exports = function (ctx) {
         // https://www.electron.build/configuration/configuration
 
         // appId: 'quasar-app'
-        win: {
-          target: 'nsis',
-          arch: [
-            'x64',
-            'ia32'
-          ]
-        }
       }
     }
   }
