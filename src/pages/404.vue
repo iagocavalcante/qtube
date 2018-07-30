b7d88a2de9b86778c8ed6296cc588c033aacf82c
Iago Cavalcante <iagoangelimc@gmail.com>
2018-07-30 11:13:57 -0300

    404

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -3,7 +3,7 @@
     <p>
       <img
         src="~assets/sad.svg"
-        style="width:30vw;max-width:150px;"
+        style="width:35vw;max-width:150px;"
       >
     </p>
     <p class="text-faded">Sorry, nothing here...<strong>(404)</strong></p>
