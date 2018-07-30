0e2df9ff930c0502c3ca50f888b1c6acd7dc9854
Iago Cavalcante <iagoangelimc@gmail.com>
2018-07-30 17:55:48 -0300

    More tests

--- src-electron/main-process/electron-main.js
+++ src-electron/main-process/electron-main.js
@@ -71,10 +71,6 @@ function createWindow () {
   autoUpdater.checkForUpdatesAndNotify()
 }
 
-// autoUpdater.on('update-downloaded', (info) => {
-//   mainWindow.webContents.send('updateReady')
-// })
-
 // when receiving a quitAndInstall signal, quit and install the new version )
 ipcMain.on('quitAndInstall', (event, arg) => {
   autoUpdater.quitAndInstall()
@@ -163,37 +159,37 @@ app.on('activate', () => {
   }
 })
 
-const sendStatusToWindow = (text) => {
-  log.info(text)
-  if (mainWindow) {
-    mainWindow.webContents.send('updateReady', text)
-  }
-}
+// const sendStatusToWindow = (text) => {
+//   log.info(text)
+//   if (mainWindow) {
+//     mainWindow.webContents.send('updateReady', text)
+//   }
+// }
 
-autoUpdater.on('checking-for-update', () => {
-  sendStatusToWindow('Checking for update...')
-})
-autoUpdater.on('update-available', info => {
-  sendStatusToWindow('Update available.')
-})
-autoUpdater.on('update-not-available', info => {
-  sendStatusToWindow('Update not available.')
-})
-autoUpdater.on('error', err => {
-  sendStatusToWindow(`Error in auto-updater: ${err.toString()}`)
-})
-autoUpdater.on('download-progress', progressObj => {
-  sendStatusToWindow(
-    `Download speed: ${progressObj.bytesPerSecond} - Downloaded ${progressObj.percent}% (${progressObj.transferred} + '/' + ${progressObj.total} + )`
-  )
-})
-autoUpdater.on('update-downloaded', info => {
-  sendStatusToWindow('Update downloaded will install now')
-})
+// autoUpdater.on('checking-for-update', () => {
+//   sendStatusToWindow('Checking for update...')
+// })
+// autoUpdater.on('update-available', info => {
+//   sendStatusToWindow('Update available.')
+// })
+// autoUpdater.on('update-not-available', info => {
+//   sendStatusToWindow('Update not available.')
+// })
+// autoUpdater.on('error', err => {
+//   sendStatusToWindow(`Error in auto-updater: ${err.toString()}`)
+// })
+// autoUpdater.on('download-progress', progressObj => {
+//   sendStatusToWindow(
+//     `Download speed: ${progressObj.bytesPerSecond} - Downloaded ${progressObj.percent}% (${progressObj.transferred} + '/' + ${progressObj.total} + )`
+//   )
+// })
+// autoUpdater.on('update-downloaded', info => {
+//   sendStatusToWindow('Update downloaded will install now')
+// })
 
-autoUpdater.on('update-downloaded', info => {
-  // Wait 5 seconds, then quit and install
-  // In your application, you don't need to wait 500 ms.
-  // You could call autoUpdater.quitAndInstall() immediately
-  autoUpdater.quitAndInstall()
-})
+// autoUpdater.on('update-downloaded', info => {
+//   // Wait 5 seconds, then quit and install
+//   // In your application, you don't need to wait 500 ms.
+//   // You could call autoUpdater.quitAndInstall() immediately
+//   autoUpdater.quitAndInstall()
+// })
